1d10b54ccce9002afa0e1779d46a572f324846cd
Arpita Roy <100989922+Arpita23r@users.noreply.github.com>
2025-02-19 13:51:43 +0530

    Create 2ndLargest.java

---
+++ 2ndLargest.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if (n < 2) {
+            System.out.println("No second largest element");
+            return;
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] < largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("No second largest element");
+        } else {
+            System.out.println(secondLargest);
+        }
+    }
+}
