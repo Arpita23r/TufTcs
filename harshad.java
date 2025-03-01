4b5eec411c26ead30ea6096865e5c9ed01294b61
Arpita Roy <100989922+Arpita23r@users.noreply.github.com>
2025-03-02 01:21:14 +0530

    Create harshad.java

---
+++ harshad.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int temp=n;
+        int sum=0;
+        while(temp!=0) {
+            sum+=temp%10;
+            temp/=10;
+        }
+        if(n%sum==0) {
+            System.out.println("Harshad");
+        }
+        else{
+            System.out.println("not harshad");
+        }
+    }
+}
