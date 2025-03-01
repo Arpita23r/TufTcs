9a9ad0c568f78a8772bc9778cc7e19bd53023bfe
Arpita Roy <100989922+Arpita23r@users.noreply.github.com>
2025-03-01 19:30:16 +0530

    Create greatestOfThree.java

---
+++ greatestOfThree.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int n3=sc.nextInt();
+        System.out.println(Math.max(n1,Math.max(n2,n3)));
+        }
+    }
