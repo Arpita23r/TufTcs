68df78098a413704ca31eff153f9be044232f254
Arpita Roy <100989922+Arpita23r@users.noreply.github.com>
2025-03-14 03:28:12 +0530

    Create concateOneStringToAnother.java

---
+++ concateOneStringToAnother.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        String str1="hello";
+        String str2="world";
+        String result=str1+str2;
+        System.out.println(result);
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        String str1="hello";
+        String str2="world";
+        String result=str1.concat(str2);
+        System.out.println(result);
+    }
+}
